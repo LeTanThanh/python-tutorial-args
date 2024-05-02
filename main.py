a1fe12212a1c46e34e6cbea6b51fa7b3d078f05a
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2024-05-02 15:44:44 +0700

    Tuple unpacking

--- main.py
+++ main.py
@@ -1,2 +1,27 @@
 if __name__ == "__main__":
-  print("Python *args")
+  # Tuple unpacking
+
+  x, y = 10, 20
+
+  print(x)
+  print(y)
+
+  def add(x, y):
+    return x + y
+
+  print(add(10, 20))
+
+  x, y, *z = 10, 20, 30, 40
+  print(x)
+  print(y)
+  print(z)
+
+  def add(x, y, *args):
+    total = x + y
+
+    for arg in args:
+      total += arg
+
+    return total
+
+  print(add(10, 20, 30, 40))
